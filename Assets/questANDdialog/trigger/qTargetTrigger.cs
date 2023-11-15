7761b32f1262f6a83cdbc59025157082ada2d5df
109a04 <fju109a04csie@gmail.com>
2023-11-15 16:21:19 +0800

    任務系統微調

--- Assets/questANDdialog/trigger/qTargetTrigger.cs
+++ Assets/questANDdialog/trigger/qTargetTrigger.cs
@@ -6,6 +6,7 @@ public class qTargetTrigger : Interactable
 {
     private QuestTarget qTarget;
     private Talkable qTalk;
+    private bool isFinish;
 
     // Start is called before the first frame update
     void Start()
@@ -13,19 +14,27 @@ public class qTargetTrigger : Interactable
         base.Start();
         qTarget = this.gameObject.GetComponent<QuestTarget>();
         qTalk = this.gameObject.GetComponent<Talkable>();
+        isFinish = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         base.Update();
-        if (isInRange && Input.GetKeyDown(KeyCode.F))
+        if (isInRange && Input.GetKeyDown(KeyCode.F) && isFinish == false)
         {
             if(qTalk != null)
             {
-                qTalk.StartTalk();
+                qTalk.StartTalk();//¥æ½Í¥ô°È
             }
             qTarget.questTargetCheck();
+            
+            Debug.Log("Hiding UI elements");
+            nameText.gameObject.SetActive(false);
+            hintText.gameObject.SetActive(false);
+            pressF.gameObject.SetActive(false);
+
+            isFinish = true;
         }
     }
 }
