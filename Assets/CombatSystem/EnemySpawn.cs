1e7db36bdc30d3ddbc5873442cf7a95945249233
Rectangle1227 <fju109a04csie@gmail.com>
2023-12-03 17:43:01 +0800

    生成怪物的腳本（啥也沒寫）

---
+++ Assets/CombatSystem/EnemySpawn.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 控制怪物生成的腳本
+/// </summary>
+
+public class EnemySpawn : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public Transform spawnPoint;
+    public Vector3 spawnPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
