76103b2e414969c876168d5f4e74a7beefc50506
109a04 <fju109a04csie@gmail.com>
2023-12-03 22:18:22 +0800

    可以正常出現在同一個場景嘞
    如題

--- Assets/multiPeopleUI/multiPeopleScript/NetworkCameraFollow.cs
+++ Assets/multiPeopleUI/multiPeopleScript/NetworkCameraFollow.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
-public class CameraFollow : MonoBehaviour
+public class NetworkCameraFollow : MonoBehaviour
 {
     public Transform player; // 玩家物件
     public float rotationSpeed = 3f; // 視角旋轉速度
@@ -11,14 +11,21 @@ public class CameraFollow : MonoBehaviour
 
     private float mouseX, mouseY; // 用於存儲滑鼠的移動
     private bool isRotating = false; // 標記是否正在旋轉
+    //public NetworkObject networkObject;
+    public NetworkCharacterControllerPrototype networkCharactorController;
 
 
     void Start()
     {
-        Vector3 direction = new Vector3(0, cameraHeight, -5); // 調整相機的高度和距離玩家的距離
-        Quaternion rotation = Quaternion.Euler(mouseY, mouseX, 0);
-        transform.position = transform.position + rotation * direction;
-        transform.LookAt(player.position);
+        
+        if (networkCharactorController)
+        {
+            Vector3 direction = new Vector3(0, cameraHeight, -5); // 調整相機的高度和距離玩家的距離
+            Quaternion rotation = Quaternion.Euler(mouseY, mouseX, 0);
+            transform.position = transform.position + rotation * direction;
+            transform.LookAt(player.transform.position);
+        }
+            
     }
 
     void Update()
@@ -28,7 +35,7 @@ public class CameraFollow : MonoBehaviour
 
     void LateUpdate()
     {
-        HandleCameraMovement();   
+        HandleCameraMovement();
     }
 
     void HandleRotationInput()
@@ -53,12 +60,15 @@ public class CameraFollow : MonoBehaviour
 
     void HandleCameraMovement()
     {
-        Vector3 direction = new Vector3(0, cameraHeight, -5); // 調整相機的高度和距離玩家的距離
-        Quaternion rotation = Quaternion.Euler(mouseY, mouseX, 0);
-        transform.position = player.position + rotation * direction;
+        
 
-        player.rotation = Quaternion.Euler(0, mouseX, 0);
-
-        transform.LookAt(player.position);
+        if (networkCharactorController)
+        {
+            Vector3 direction = new Vector3(0, cameraHeight, -5); // 調整相機的高度和距離玩家的距離
+            Quaternion rotation = Quaternion.Euler(mouseY, mouseX, 0);
+            transform.position = transform.position + rotation * direction;
+            player.rotation = Quaternion.Euler(0, mouseX, 0);
+            transform.LookAt(player.transform.position);
+        }
     }
 }
