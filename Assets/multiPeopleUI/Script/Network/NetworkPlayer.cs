45c5a1065d3dc26cc273f505861a5a37fbaa829a
yus091 <ysheng910316@gmail.com>
2023-12-17 01:18:07 +0800

    多人模式的貓頭上有字了

--- Assets/multiPeopleUI/Script/Network/NetworkPlayer.cs
+++ Assets/multiPeopleUI/Script/Network/NetworkPlayer.cs
@@ -2,12 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Fusion;
+using TMPro;
 
 public class NetworkPlayer : NetworkBehaviour, IPlayerLeft
 {
+    public TextMeshProUGUI playerNickNameTM;
+    //public string playerNickName;
+    //public playerNameManager playerNameManager;
+    //public playerAttributeManager PlayerAttributeManager;
     public static NetworkPlayer Local { get; set; }
     // 方便去查找誰才是 local player
     // Start is called before the first frame update
+
+    [Networked(OnChanged = nameof(OnNickNameChanged))]
+    public NetworkString<_16> nickName { get; set; }
+
     void Start()
     {
         
@@ -22,6 +31,8 @@ public class NetworkPlayer : NetworkBehaviour, IPlayerLeft
             // 如果不檢查的話就會像之前那樣在其中一個視窗移動，所有的玩家都會被移動
             Local = this;
             Debug.Log("生成了 local player");
+            RPC_SetNickName(playerAttributeManager.Instance.pname);
+            Debug.Log($"名字是 {playerAttributeManager.Instance.pname}");
 
         }
         else
@@ -48,5 +59,27 @@ public class NetworkPlayer : NetworkBehaviour, IPlayerLeft
             
     }
 
+    static void OnNickNameChanged(Changed<NetworkPlayer> changed)
+    {
+        Debug.Log($"{Time.time} OnHPChanged value {changed.Behaviour.nickName}");
 
+        changed.Behaviour.OnNickNameChanged();
+    }
+    private void OnNickNameChanged()
+    {
+        Debug.Log($"Nick name changed for player to {nickName} for player {gameObject.name}");
+
+        playerNickNameTM.text = nickName.ToString();
+        //playerNickName.text = nickName.ToString();
+    }
+
+    [Rpc(RpcSources.InputAuthority, RpcTargets.StateAuthority)]
+
+    public void RPC_SetNickName(string nickName, RpcInfo info = default)
+    {
+        Debug.Log($"[RPC] SetNickName {nickName}");
+        this.nickName = nickName;
+
+
+    }
 }
